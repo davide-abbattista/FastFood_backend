8e93be602a8aa6057907f4c44395d1b9155bc6fd
davide-abbattista <davide27abbattista@gmail.com>
2022-06-07 12:39:15 +0200

    Update

--- controllers/products.js
+++ controllers/products.js
@@ -6,9 +6,7 @@ module.exports = {
             .then(products => {
                 res.status(200).json(products);
             })
-            .catch(error => {
-                res.status(500).json(error);
-            })
+            .catch(error => res.status(500).json(error))
     },
 
     addProduct: (req, res, next) => {
@@ -17,7 +15,7 @@ module.exports = {
                 res.status(500).json(error);
             } else {
                 console.log(`${savedProduct} inserito`);
-                res.status(200).json(savedProduct);
+                res.status(200).json(`Il prodotto con id: ${savedProduct.productId} è stato inserito`);
             }
         });
     },
@@ -28,28 +26,26 @@ module.exports = {
                 res.locals.id = product._id;
                 next();
             })
-            .catch(error => {
-                res.status(500).json(error);
-            })
+            .catch(error => res.status(500).json(error))
     },
 
     updateProduct: (req, res, next) => {
         Product.findByIdAndUpdate(res.locals.id, {
             $set: req.body
         },null).exec()
-            .then(updatedProduct => {
-                console.log(`${updatedProduct} aggiornato`);
-                res.status(200).json(updatedProduct);
+            .then(oldProduct => {
+                console.log(`${oldProduct} aggiornato`);
+                res.status(200).json(`Il prodotto con id: ${oldProduct.productId} è stato aggiornato`);
             })
-            .catch(error => {
-                console.log(error);
-                res.status(500).json(error);
-            })
+            .catch(error => res.status(500).json(error))
         },
 
     deleteProduct: (req, res, next) => {
         Product.findByIdAndDelete(res.locals.id).exec()
-            .then(deletedProduct => res.status(200).json(`${deletedProduct} rimosso`))
+            .then(deletedProduct => {
+                console.log(`${deletedProduct} rimosso`);
+                res.status(200).json(`Il prodotto con id: ${deletedProduct.productId} è stato rimosso`);
+            })
             .catch(error => res.status(500).json(error));
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express'),
     ordersRouter = require('./orders'),
-    productsRouter = require('./products');
+    productsRouter = require('./products'),
+    methodOverride = require('method-override');
 
 const router = express.Router();
 
+router.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}));
 router.use('/api/orders', ordersRouter);
 router.use('/api/products', productsRouter);
 
